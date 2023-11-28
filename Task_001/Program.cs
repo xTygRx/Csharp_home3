d6b90e4656c2da047066cbc8552ea0a7ecbf4c78
divinged@inbox.ru <divinged@inbox.ru>
2023-11-28 12:23:05 +0300

    Добавили решение 1 задачи

---
+++ Task_001/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] GenerateArray(int size, int leftRange, int rigthRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+        {
+        tempArray[i] = rand.Next(leftRange, rigthRange + 1);
+        }
+    return tempArray;
+}
+
+void PrintArray(int[] array2){
+System.Console.WriteLine("[" + string.Join(", ", array2) + "]");
+}
+
+int size = 10;
+int count = 0;
+int[] array = GenerateArray(size, 1, 100);
+PrintArray(array);
+
+for( int i = 0; i < array.Length; i++){
+    if (array[i] >= 20 && array[i] <= 90)
+    count++;
+}
+System.Console.WriteLine($"Количество элементов массива значения, значения которых лежат на отрезке [20,90] {count}");
